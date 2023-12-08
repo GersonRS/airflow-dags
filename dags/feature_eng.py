06dfde445a4108dd082151d665253237c65d4e69
GersonRS <grs@cesar.org.br>
2023-12-08 03:18:16 -0300

    Fix xcom values

--- dags/feature_eng.py
+++ dags/feature_eng.py
@@ -171,6 +171,18 @@ def feature_eng():
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+        X_train_df = pd.DataFrame(X_train, columns=X.columns)
+        X_train_df.index = X_train.index
+
+        X_test_df = pd.DataFrame(X_test, columns=X.columns)
+        X_test_df.index = X_test.index
+
+        y_train_df = pd.DataFrame({"target": y_train})
+        y_train_df.index = X_train.index
+
+        y_test_df = pd.DataFrame({"target": y_test})
+        y_test_df.index = X_test.index
+
         return {
             "X_train": X_train,
             "X_test": X_test,
