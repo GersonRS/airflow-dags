402b84f879a6754b49afcfdcc30ccb8c974ca4bc
GersonRS <gersonrodriguessantos8@gmail.com>
2023-12-21 22:34:58 -0300

    Adjust requirements and ci

--- dags/feature_eng.py
+++ dags/feature_eng.py
@@ -119,7 +119,9 @@ def feature_eng() -> None:
         experiment_already_exists = EmptyOperator(task_id="experiment_exists")
 
         @task(trigger_rule="none_failed")
-        def get_current_experiment_id(experiment_name: str, max_results: int = 1000) -> Any:
+        def get_current_experiment_id(
+            experiment_name: str, max_results: int = 1000
+        ) -> Any:
             "Get the ID of the specified MLFlow experiment."
 
             mlflow_hook = MLflowClientHook(mlflow_conn_id=MLFLOW_CONN_ID)
@@ -191,7 +193,9 @@ def feature_eng() -> None:
 
         logging.info(pd.concat([X, y], axis=1).head())
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42
+        )
 
         X_train_df = pd.DataFrame(X_train, columns=X.columns)
         X_train_df.index = X_train.index
