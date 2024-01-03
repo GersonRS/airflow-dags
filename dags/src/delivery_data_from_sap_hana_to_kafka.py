bf35402d87b6c84581b37c674885957cb0459c53
GersonRS <gersonrodriguessantos8@gmail.com>
2024-01-03 13:15:03 -0300

    Config repository

--- dags/src/delivery_data_from_sap_hana_to_kafka.py
+++ dags/src/delivery_data_from_sap_hana_to_kafka.py
@@ -1,20 +1,7 @@
-import json
-import logging
-import time
-from logging import log
 from sys import argv
 
-import functions_global as fn
-import pymssql
-from confluent_kafka import avro
-from confluent_kafka.avro import AvroProducer
-from minio import Minio
-from minio.error import S3Error
-from pyspark import *
-from pyspark.sql import SparkSession, Window, types
-from pyspark.sql.avro.functions import to_avro
-from pyspark.sql.functions import current_timestamp, desc, lit, struct, to_json
-from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp, lit, struct, to_json
 
 
 # função para executar o job
@@ -22,20 +9,20 @@ def executa():
     # Ler a tabela usando PySpark
     df = (
         spark.read.format("jdbc")
-        .option("url", "jdbc:sap://10.163.9.4:30041/HAQ")
-        .option("dbtable", "SAPHANADB.CRCO")
-        .option("user", "SYNAPSE_READ")
-        .option("password", "Syn@ps322SAP22")
-        .option("loginTimeout", 60)
-        .option("driver", "com.sap.db.jdbc.Driver")
+        .option("url", argv[1].replace('jdbc_url="', "").replace('"', ""))
+        .option("user", argv[2].replace('user_sap="', "").replace('"', ""))
+        .option("password", argv[3].replace('password_sap="', "").replace('"', ""))
+        .option("dbtable", argv[4].replace('dbtable_sap="', "").replace('"', ""))
         .option("fetchSize", "10000")
+        .option("driver", "com.sap.db.jdbc.Driver")
+        .option("loginTimeout", 60)
         .load()
     )
 
     df_processed = (
         df.withColumn("ingestion_time", lit(current_timestamp()))
         .withColumn("source_system", lit("sap"))
-        .withColumn("user_name", lit("gersonrs"))
+        .withColumn("user_name", lit("airflow"))
         .withColumn("ingestion_type", lit("spark"))
         .withColumn("base_format", lit("table"))
         .withColumn("rows_written", lit(df.count()))
@@ -50,8 +37,10 @@ def executa():
         df_processed.select(to_json(struct("*")).alias("value"))
         .selectExpr("CAST(value AS STRING)")
         .write.format("kafka")
-        .option("kafka.bootstrap.servers", "20.84.11.171:9094")
-        .option("topic", "topic-test")
+        .option(
+            "kafka.bootstrap.servers", argv[5].replace('kafka_bootstrap="', "").replace('"', "")
+        )
+        .option("topic", argv[6].replace('kafka_topic="', "").replace('"', ""))
         .save()
     )
 
