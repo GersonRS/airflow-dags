12cb7975225dbbdad9f1779f9779193be3618dac
GersonRS <gersonrodriguessantos8@gmail.com>
2024-02-03 17:08:14 -0300

    fix: remove some unnecessary codes

--- dags/feature_eng.py
+++ dags/feature_eng.py
@@ -171,19 +171,11 @@ def feature_eng() -> None:
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        X_train_df = pd.DataFrame(X_train, columns=X.columns)
-        X_train_df.index = X_train.index
-        X_test_df = pd.DataFrame(X_test, columns=X.columns)
-        X_test_df.index = X_test.index
-        y_train_df = pd.DataFrame({"target": y_train})
-        y_train_df.index = X_train.index
-        y_test_df = pd.DataFrame({"target": y_test})
-        y_test_df.index = X_test.index
         return {
-            "X_train": X_train_df,
-            "X_test": X_test_df,
-            "y_train": y_train_df,
-            "y_test": y_test_df,
+            "X_train": X_train,
+            "X_test": X_test,
+            "y_train": y_train,
+            "y_test": y_test,
         }
 
     input_table = Table(
